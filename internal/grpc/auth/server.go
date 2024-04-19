235627bbcf302b12add0a9a258c0bd107afdacee
1eanq <153944563+1eanq@users.noreply.github.com>
2024-04-20 01:23:00 +0700

    Add files via upload

---
+++ internal/grpc/auth/server.go
@@ -0,0 +1,93 @@
+package auth
+
+import (
+	genv1 "GRPC_Calc/proto/gen"
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type Auth interface {
+	Login(ctx context.Context,
+		email string,
+		password string,
+	) (token string, err error)
+
+	RegisterNewUser(ctx context.Context,
+		email string,
+		password string,
+	) (userID int64, err error)
+}
+
+type serverAPI struct {
+	genv1.UnimplementedCalcServer
+	auth Auth
+}
+
+func Register(gRPC *grpc.Server, auth Auth) {
+	genv1.RegisterCalcServer(gRPC, &serverAPI{auth: auth})
+}
+
+func (s *serverAPI) Login(
+	ctx context.Context,
+	req *genv1.LoginRequest,
+) (*genv1.LoginResponse, error) {
+
+	if err := validateLogin(req); err != nil {
+		return nil, err
+	}
+
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		// TODO: ...
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &genv1.LoginResponse{
+		Token: token,
+	}, nil
+}
+
+func validateLogin(req *genv1.LoginRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	return nil
+}
+
+func validateRegister(req *genv1.RegisterRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	return nil
+}
+
+func (s *serverAPI) Register(
+	ctx context.Context,
+	req *genv1.RegisterRequest,
+) (*genv1.RegisterResponse, error) {
+	if err := validateRegister(req); err != nil {
+		return nil, err
+	}
+
+	userID, err := s.auth.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		//TODO: ...
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &genv1.RegisterResponse{
+		UserId: userID,
+	}, nil
+}
